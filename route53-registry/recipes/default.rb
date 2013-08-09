51d2a945d4a494a5e8ca9a1001ed1c7bfcad4f27
Cameron Bytheway <cameron@theflokk.com>
2013-08-08 20:00:34 -0600

    Try out another route53 client

--- route53-registry/recipes/default.rb
+++ route53-registry/recipes/default.rb
@@ -1,13 +1,8 @@
-include_recipe "route53"
+client = Route53::Client.new(node[:route53_registry][:aws_access_key_id], node[:route53_registry][:aws_secret_access_key])
 
-route53_record "create a record" do
-  name  node[:route53_registry][:record]
-  value node[:opsworks][:instance][:private_ip]
-  type  "A"
+name = node[:route53_registry][:name]
+value = node[:opsworks][:instance][:private_ip]
+type = "A"
+zone_id = node[:route53_registry][:zone_id]
 
-  zone_id               node[:route53_registry][:zone_id]
-  aws_access_key_id     node[:route53_registry][:aws_access_key_id]
-  aws_secret_access_key node[:route53_registry][:aws_secret_access_key]
-
-  action :create
-end
+client.create_or_append_record(name, value, type, zone_id)
